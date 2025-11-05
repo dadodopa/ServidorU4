5e658a661b2c380c3ca6c0717703c3c94cc0915d
dadodopa <dopazodominguez@gmail.com>
2025-11-05 09:13:53 +0100

    05/11/2025

---
+++ Explicacion_Cookies/Ejemplo2/paginaDestino.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Destino
+        <?php
+        echo "<pre>";
+        echo "Cookie ";
+        print_r($_COOKIE);
+        setcookie("otroValor", "Eva"); //Este dato no estará en Cookie, hasta la siguiente petición al sitio.
+        
+        echo "Cookie ";
+        print_r($_COOKIE);
+        
+        
+        //setCookie("otroValor", "Eva", time()-3000);
+        
+        echo "Cookie "; //Comprobamos si en Cookie está apellido, el valor debería de ser MiApellido
+        print_r($_COOKIE);
+        
+        echo "REQUEST "; //Comprobamos si en el request está apellido, el valor debería de ser el que introduce el usuario.
+        print_r($_REQUEST);
+        //Aunque tengamos el mismo nombre para una Cookie que para una control del formulario no existe en la actualidad ningún problema, como ocurría en versiones anteriores de PHP. 
+        //Desde la versión 5.3.0 no se guardan por defecto los datos de las Cookies en el REQUEST.
+        
+        echo "</pre>";
+        ?>
+    </body>
+</html>
