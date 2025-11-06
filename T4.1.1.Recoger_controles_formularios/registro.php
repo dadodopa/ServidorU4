30f0cdae227847dcdb95181c127a9a38635517ee
dadodopa <dopazodominguez@gmail.com>
2025-11-06 14:36:09 +0100

    06/11/2025_1

--- T4.1.1.Recoger_controles_formularios/registro.php
+++ T4.1.1.Recoger_controles_formularios/registro.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+$_SESSION["nombre"]="Daniel";
+//unset($_SESSION["nombre"]);//Pedemos eliminar un valor de la session
+echo "<pre>";
+print_r($_SESSION); 
+echo "Identificador ". session_id();
+echo "Estado 1des - 2hab: ". session_status();
+echo "</pre>";
+session_reset();//Para vaciar la sesion
+session_destroy();//Para destruir la session
+echo "<pre>";
+print_r($_SESSION);
+echo "Identificador ". session_id();
+echo "Estado 1des - 2hab: ". session_status();
+echo "</pre>";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
