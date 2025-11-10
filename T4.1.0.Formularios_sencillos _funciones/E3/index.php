5b80de80992d7869aa0cb76ef23d48ce5b1ea3de
dadodopa <dopazodominguez@gmail.com>
2025-11-10 20:31:33 +0100

    10/11/2025

--- T4.1.0.Formularios_sencillos _funciones/E3/index.php
+++ T4.1.0.Formularios_sencillos _funciones/E3/index.php
@@ -1,12 +1,55 @@
 <?php
-function generarPassNum($min,$max) {
-    $finalString=[];
-    $maxPassword = rand($min,$max);
-    for ($i=0; $i < $maxPassword; $i++) { 
-        $charPassword = rand(0,9);
-        $finalString .= $charPassword;
+$minPass = $_POST['min'];
+$maxPass = $_POST['max'];
+
+switch ($_POST['tipo']) {
+    case 'letters':
+        echo "<h1>Tu contraseña es</h1><p>" . generarPassAlfa($minPass, $maxPass) . "</p>";
+        break;
+    case 'number':
+        echo "<h1>Tu contraseña es</h1><p>" . generarPassNum($minPass, $maxPass) . "</p>";
+        break;
+    case 'full':
+        echo "<h1>Tu contraseña es</h1><p>" . generarPassAlfaNum($minPass, $maxPass) . "</p>";
+        break;
+    default:
+        echo "<h1>Se ha producido un error</h1>";
+        break;
+}
+
+// Solo números
+function generarPassNum($min, $max) {
+    $finalString = "";
+    $length = rand($min, $max);
+    for ($i = 0; $i < $length; $i++) {
+        $finalString .= rand(0, 9);
+    }
+    return $finalString;
+}
+
+// Solo letras
+function generarPassAlfa($min, $max) {
+    $finalString = "";
+    $letters = range('a', 'z');
+    $length = rand($min, $max);
+    for ($i = 0; $i < $length; $i++) {
+        $finalString .= $letters[rand(0, 25)];
+    }
+    return $finalString;
+}
+
+// Letras y números
+function generarPassAlfaNum($min, $max) {
+    $finalString = "";
+    $letters = range('a', 'z');
+    $length = rand($min, $max);
+    for ($i = 0; $i < $length; $i++) {
+        if (rand(1, 2) == 1) {
+            $finalString .= $letters[rand(0, 25)];
+        } else {
+            $finalString .= rand(0, 9);
+        }
     }
     return $finalString;
 }
 ?>
-
--- T4.1.1.Recoger_controles_formularios/agente.php
+++ T4.1.1.Recoger_controles_formularios/agente.php
@@ -1,6 +1,14 @@
 <?php
-session_start();
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
+    echo "<p>El nombre completo es: </p>".$_POST["fullname"];
+    echo "<p>El nombre de usuario es: </p>".$_POST["userName"];
+    echo "<p>La contraseña es: </p>".$_POST["password"];
+    echo "<p>La edad es:</p>".$_POST["age"];
+    echo "<p>El cumpleaños es: </p>".$_POST["birthday"];
+    echo "<p>El correo es: </p>".$_POST["email"];
+    echo "<p>La URL personal es: </p>".$_POST["personalUrl"];
+    echo "<p>La IP del equipo es: </p>".$_POST["pcIP"];
+    echo "<p>Los hobbies son: </p>".$_POST["hobbies"];
+    if (condition) {
+        # code...
+    }
 ?>
